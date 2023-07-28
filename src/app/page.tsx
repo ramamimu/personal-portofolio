32f7f63db65f4bbdaa9bd0094dec3a1d7557566c
ramamimu <ahhmaad.blogger@gmail.com>
2023-07-29 01:11:48 +0700

    feat: husky pre-commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { useRef, useEffect } from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="container mx-auto">
+        <p>Hello World</p>
+      </div>
+    </main>
+  );
+}
