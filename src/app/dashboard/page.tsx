f69e903e71ea2cdcc9eebf29aaba9347bacaef96
ramamimu <ahhmaad.blogger@gmail.com>
2023-07-30 16:02:29 +0700

    feat: first appearing page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,46 @@
+"use client";
+import { motion, AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+import Layout from "@/components/mainLayout";
+import Link from "next/link";
+
 export default function Dashboard() {
-  return <p>Dashboard</p>;
+  const pathName = usePathname();
+
+  return (
+    <Layout>
+      <AnimatePresence mode="sync">
+        <motion.div
+          key={pathName}
+          initial={{
+            opacity: 0,
+            clipPath:
+              "polygon(0 100%, 100% 98%, 100% 50%, 100% 100%, 50% 100%, 0 100%)",
+          }}
+          animate={{
+            opacity: 1,
+            clipPath:
+              "polygon(0 0, 100% 0, 100% 50%, 100% 100%, 0 100%, 0% 50%)",
+          }}
+          exit={{
+            clipPath:
+              "polygon(0 100%, 100% 98%, 100% 50%, 100% 100%, 50% 100%, 0 100%)",
+          }}
+          transition={{ duration: 1 }}
+        >
+          <Link href="/">click me</Link>
+          {/* <motion.div
+            className="h-96 w-96 rounded-3xl bg-blue-400 p-10"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            Dashboard
+          </motion.div> */}
+          hehe
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,43 @@
-import { useRef, useEffect } from "react";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { motion, AnimatePresence } from "framer-motion";
+
+import Layout from "@/components/mainLayout";
+import FirstAppear from "@/components/firstAppear";
+
+export default function Dashboard() {
+  const pathName = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <main>
-      <div className="container mx-auto">
-        <p>Hello World</p>
+    <Layout>
+      <FirstAppear />
+      <div className="bg-orange-400 p-10">
+        <AnimatePresence mode="wait">
+          {isOpen && (
+            <motion.div
+              className="h-96 w-96 rounded-3xl bg-blue-400 p-10"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              Dashboard
+            </motion.div>
+          )}{" "}
+        </AnimatePresence>
+        <p
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          play the box
+        </p>
       </div>
-    </main>
+    </Layout>
   );
 }
