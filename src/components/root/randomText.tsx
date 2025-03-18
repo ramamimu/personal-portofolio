657aad30c3f06cecaa5062b3e2425c2fb52981e3
ramamimu <ahm.f.ramadhani@gmail.com>
2025-03-18 23:10:34 +0700

    breaking-changes: move from react -> nuxt

--- src/components/root/randomText.tsx
+++
@@ -1,7 +0,0 @@
-export default function RandomText() {
-  return (
-    <section className="absolute -bottom-16 right-24 font-monserrat text-6xl font-bold text-white">
-      <p>The Journey has Begun</p>
-    </section>
-  );
-}
