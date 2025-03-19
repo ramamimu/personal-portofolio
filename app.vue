ae93b3354d5687a00e80f780e2b6ca86f070c5fb
ramamimu <ahm.f.ramadhani@gmail.com>
2025-03-19 21:39:45 +0700

    feat: initial commi

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-   <h1 class="text-7xl">Hello world</h1>
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="grid grid-cols-8">
+    <section class="col-span-8 md:col-span-3">
+      <div>
+        <div>
+          <img src="/assets/images/icons/github.svg" alt="">
+          <img src="/assets/images/icons/linkedin.svg" alt="">
+        </div>
+        <h1>An Introduction</h1>
+        <p>
+          This work serves as documentation of my journey as a software engineer. It is my personal reward—a reminder of how far I've come.
+        </p>
+        <p>
+          In this project, you'll explore my experiences in front-end, back-end, and cloud development. I work with Vue, React, Go, Node.js, Python, and Google Cloud Platform (GCP), and this journey reflects my growth, challenges, and accomplishments in these technologies.
+        </p>
+        <p>
+          If you're short on time to enjoy the full details, feel free to download my CV below.
+        </p>
+      </div>
+    </section>
+    <section class="col-span-8 md:col-span-2">
+      <div>
+        <p>“Give glad tidings and do not repel people. Make things easy and do not make things difficult”</p>
+        <p>Sahih al-Bukhari, 3559</p>
+      </div>
+      <div>
+        <p>"A man who stands for nothing will fal for anything"</p>
+        <p>Malcolm X</p>
+      </div>
+    </section>
+    <section class="col-span-8 md:col-span-2">
+      <img src="/assets/images/introduction/me.png" alt="">
+    </section>
+  </div>
+</template>
